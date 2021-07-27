ee2a8bc7152b54063ca34a10ba94a26858962720
krishna-elear <53559967+krishna-elear@users.noreply.github.com>
2021-07-28 01:04:12 +0530

    completes basic POC

--- app/src/main/java/buzz/getcoco/example/simplechatexample/MainActivity.java
+++ app/src/main/java/buzz/getcoco/example/simplechatexample/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import buzz.getcoco.api.CocoClient;
 import buzz.getcoco.api.DefaultNativeCallbacksInterface;
@@ -15,16 +18,77 @@ import buzz.getcoco.api.PlatformInterface;
 public class MainActivity extends AppCompatActivity {
 
   private static final String TAG = "MainActivity";
+  private static final String NETWORK_ID = "5eb99586c46daa001080a24b";
 
   static {
     System.loadLibrary("cocojni");
   }
 
-  private static void init(File dir) {
-    Log.d(TAG, "init: started");
+  private interface ThrowingRunnable {
+    void run() throws Throwable;
+  }
+
+  private static void runOnNewThread(ThrowingRunnable runnable) {
+    new Thread(() -> {
+      try {
+        runnable.run();
+      } catch (Throwable tr) {
+        Log.e(TAG, "runOnNewThread: error", tr);
+      }
+    }).start();
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    firstStep(getFilesDir(), getString(R.string.client_id));
+  }
+
+  private static void setTokens() {
+
+    try {
+
+      CocoClient.getInstance().setTokens("" +
+          "{\n" +
+          "    \"access_token\": \"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjYwZmNhOTgwZTc4MThiMDAxNjQyZWUwZCJ9.eyJpYXQiOjE2Mjc0MTM5NzYsImV4cCI6MTYyNzQxNzU3Niwic3ViIjoiNWViZDE1YTBlNmY0MmIwMDExN2RmMTBlIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwIiwiYXVkIjoiZjA3OTliZGVhM2FlYWRlYjc0MjkiLCJzY29wZSI6WyJhcHAubWdtdCIsImRldmljZS5tZ210IiwibmV0d29yay5tZ210IiwibmV0d29yay5hY2Nlc3MiLCJwcm9maWxlLmFjY2VzcyIsInByb2ZpbGUubWdtdCJdLCJwcm9maWxlIjp7InJvbGVzIjpbIkNvbnN1bWVyIiwiRGV2ZWxvcGVyIiwiT2VtIl19LCJvcGVyYXRvcklkIjoiNWU3MzM4MzhmNmE5NWUwMDE2ZTc5MDJmIiwianRpIjoiOGEwYTBlYjlmMTM0MzFhNCJ9.kjyfJmQpmKzkrgCFXWtssIVlr-nVbniievkusirAjcxi0sK_V1rynugMW6lGm4xLdcZ8QU-X_lfES0Rqho3qOPWQGBwi10PUZRbgU0L3r1fxI-5Df5nUuRwchpyNW8vNRF9zwCIhCahzg1DrdZJJ-gXGkS0gR7mSqRaqLHkc_Yy9Dw924vRaEQrd7dj0UNXtR3ATjhskNlPzEajyoo91qXeD03L986IQnk9lQu7Y4PKVrX0IpY_jIXLs3qOaz7MAsIyuH1ogaDY5dzLvGovQhiMHhTy2Ro9g7EKGksVzry2-4mbUHHG7dvIqlWJ40TpIWFDfn5kcALqAc6CrPS76TQ\",\n" +
+          "    \"refresh_token\": \"Dq1oYnEi0T7oT_-YTI0y\",\n" +
+          "    \"expires_in\": 3600,\n" +
+          "    \"token_type\": \"Bearer\"\n" +
+          "}"
+      );
+
+    } catch (Exception e) {
+      Log.e(TAG, "setTokens: error", e);
+    }
+  }
+
+  private static void firstStep(File dir, String clientId) {
+    Log.d(TAG, "firstStep: started");
+    Log.d(TAG, "firstStep: clientId: " + clientId);
 
     new CocoClient.Builder()
         .addCallbackListener(new DefaultNativeCallbacksInterface() {
+
+          @Override
+          public void connectStatusCallback(Network network, Object context) {
+            if (!network.getId().equals(NETWORK_ID))
+              return;
+
+            Log.d(TAG, "connectStatusCallback: status: " + network.getState());
+
+            if (!network.getState().isConnected())
+              return;
+
+            runOnNewThread(MainActivity::fourthStep);
+          }
+
+          @Override
+          public void networkListCallback(ArrayList<Network> networksList, Object context) {
+            runOnNewThread(() -> thirdStep(networksList));
+          }
+
           @Override
           public void receiveDataCallback(Network network, long sourceNodeId, String data) {
             Log.d(TAG, String.format("receiveDataCallback: nodeId: %d, data: %s", sourceNodeId, data));
@@ -32,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
 
           @Override
           public void contentInfoCallback(Network network, long sourceNodeId, long contentTime, String data) {
-            Log.d(TAG, String.format("receiveDataCallback: contentTime: %d, nodeId: %d, data: %s", contentTime, sourceNodeId, data));
+            Log.d(TAG, String.format("contentInfoCallback: contentTime: %d, nodeId: %d, data: %s", contentTime, sourceNodeId, data));
           }
         })
         .withPlatform(new PlatformInterface() {
@@ -44,8 +108,13 @@ public class MainActivity extends AppCompatActivity {
           }
 
           @Override
+          public String getDownloadPath() {
+            return dir.getAbsolutePath();
+          }
+
+          @Override
           public String getClientId() {
-            return "f0799bdea3aeadeb7429";
+            return clientId;
           }
 
           @Override
@@ -55,31 +124,56 @@ public class MainActivity extends AppCompatActivity {
 
           @Override
           public void OAuthCallback(String authorizationEndpoint, String tokenEndpoint) {
-
-            try {
-              CocoClient.getInstance().setTokens("" +
-                  "{\n" +
-                  "    \"access_token\": \"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjYwZmNhOTgwOGNjY2EzMDAxNjExYmY1NyJ9.eyJpYXQiOjE2Mjc0MDU5OTQsImV4cCI6MTYyNzQwOTU5NCwic3ViIjoiNWUwOWNiZWVkMTNlMDkwMDE3YzQ2Y2U3IiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwIiwiYXVkIjoiZjA3OTliZGVhM2FlYWRlYjc0MjkiLCJzY29wZSI6WyJhcHAubWdtdCIsImRldmljZS5tZ210IiwibmV0d29yay5tZ210IiwibmV0d29yay5hY2Nlc3MiLCJwcm9maWxlLmFjY2VzcyIsInByb2ZpbGUubWdtdCJdLCJwcm9maWxlIjp7InJvbGVzIjpbIkNvbnN1bWVyIiwiRGV2ZWxvcGVyIiwiT2VtIl19LCJvcGVyYXRvcklkIjoiNWU3MzM4MzhmNmE5NWUwMDE2ZTc5MDJmIiwianRpIjoiMTJhNWVjODM3MDI2N2ZiNiJ9.MTBWeI85udWgwkWVACZSMoVJf6XJjhPAN0umUfSis24moHQbEOnWXrTKGPFzyycxnTiztp_rn1SvDX0z3g0zFTYUeMMJoy5vr19QAsRdRG6ylr-vMZ90BYjXmBWvI3DX52UYmsxOu9yVv94IttnKtKSyuVZoNjRARxoCHO3ttf6Lzs_hScvip87xJqpzaleSRU7tZ0ohbHQ_Nte_-DUHhnYKWx3rbnkRN4RXyz1ASZxYpwurRzJgyWxnq5WHK6jcCL_6xJNPaFZuJWps_YHvrrUgZq2ZVF0TEWG-DNj0-OjkQV1rRpx4jk35pa-r6RQCxlQ7RiWo6tU1GcOy7mrJsw\",\n" +
-                  "    \"refresh_token\": \"XodLWc3XSJgtepJQg2S-\",\n" +
-                  "    \"expires_in\": 3600,\n" +
-                  "    \"token_type\": \"Bearer\"\n" +
-                  "}"
-              );
-            } catch (Exception e) {
-              Log.e(TAG, "OAuthCallback: error", e);
-            }
+            runOnNewThread(MainActivity::setTokens);
           }
         })
         .build();
 
-    Log.d(TAG, "init: completed");
+    setTokens();
+
+    Log.d(TAG, "firstStep: completed");
+
+    runOnNewThread(MainActivity::secondStep);
   }
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+  private static void secondStep() throws Throwable {
+    Log.d(TAG, "secondStep: get all networks");
+    CocoClient.getInstance().getAllNetworks(null);
+  }
 
-    init(getCacheDir());
+  private static void thirdStep(List<Network> networks) {
+
+    try {
+      Thread.sleep(1_000);
+
+      if (null == networks) {
+        Log.e(TAG, "networkListCallback: error while get all networks");
+        return;
+      }
+
+      for (Network n : networks) {
+
+        if (n.getId().equals(NETWORK_ID)) {
+          n.connect();
+          break;
+        }
+      }
+    } catch (Throwable e) {
+      Log.w(TAG, "thirdStep: error", e);
+    }
+  }
+
+  private static void fourthStep() throws Exception {
+
+    Thread.sleep(5_000);
+
+    Network n = CocoClient.getInstance().getNetwork(NETWORK_ID);
+
+    Log.d(TAG, "fourthStep: using network: " + n);
+
+    Objects.requireNonNull(n);
+
+    n.sendData("hello world", null);
+    n.sendContentInfo(System.currentTimeMillis(), "world hello", null);
   }
 }
