e60dc9599ff49ab607bf9c60798ebf7d43bc2058
krishna-elear <53559967+krishna-elear@users.noreply.github.com>
2021-07-28 19:48:36 +0530

    mvp

--- app/src/main/java/buzz/getcoco/example/simplechatexample/MainActivity.java
+++ app/src/main/java/buzz/getcoco/example/simplechatexample/MainActivity.java
@@ -1,13 +1,17 @@
 package buzz.getcoco.example.simplechatexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.MutableLiveData;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 
 import buzz.getcoco.api.CocoClient;
@@ -18,7 +22,10 @@ import buzz.getcoco.api.PlatformInterface;
 public class MainActivity extends AppCompatActivity {
 
   private static final String TAG = "MainActivity";
-  private static final String NETWORK_ID = "5eb99586c46daa001080a24b";
+
+  private static final MutableLiveData<String> messageLiveData = new MutableLiveData<>();
+
+  private static String networkId;
 
   static {
     System.loadLibrary("cocojni");
@@ -43,60 +50,183 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    firstStep(getFilesDir(), getString(R.string.client_id));
+    EditText etNodeId = findViewById(R.id.et_node_id);
+    EditText etNetworkId = findViewById(R.id.et_network_id);
+    EditText etInviteURL = findViewById(R.id.et_invite_url);
+    Button btnJoin = findViewById(R.id.btn_join);
+
+    Objects.requireNonNull(etNodeId);
+    Objects.requireNonNull(etNetworkId);
+    Objects.requireNonNull(etInviteURL);
+    Objects.requireNonNull(btnJoin);
+
+    EditText etDestNodeId = findViewById(R.id.et_destination_node_id);
+    Button btnSendData = findViewById(R.id.btn_send_data);
+    Button btnSendInfo = findViewById(R.id.btn_send_content_info);
+
+    Objects.requireNonNull(etDestNodeId);
+    Objects.requireNonNull(btnSendData);
+
+    EditText etToken = findViewById(R.id.et_token);
+    Button btnSet = findViewById(R.id.btn_set);
+
+    Objects.requireNonNull(etToken);
+    Objects.requireNonNull(btnSet);
+
+    btnSet.setOnClickListener(v -> {
+      String token = etToken.getEditableText().toString();
+
+      Log.d(TAG, "onCreate: using token: " + token);
+
+      runOnNewThread(() -> CocoClient.getInstance().setTokens(token));
+    });
+
+    btnJoin.setOnClickListener(v -> {
+      String nodeIdStr = etNodeId.getEditableText().toString();
+      String networkId = etNetworkId.getEditableText().toString();
+      String inviteURL = etInviteURL.getEditableText().toString();
+
+      Log.d(TAG, "onCreate: using nodeId: " + nodeIdStr);
+      Log.d(TAG, "onCreate: inviteURL: " + inviteURL);
+      Log.d(TAG, "onCreate: networkId: " + networkId);
+
+      if (TextUtils.isEmpty(networkId) || TextUtils.isEmpty(inviteURL) || TextUtils.isEmpty(nodeIdStr)) {
+        Network net;
+
+        Toast.makeText(this, "using saved networks to join", Toast.LENGTH_LONG).show();
+
+        try {
+          Network[] nets = CocoClient.getInstance().getSavedNetworks();
+
+          if (null == nets || 1 != nets.length) {
+            throw new IllegalArgumentException();
+          }
+
+          net = nets[0];
+          net.connect();
+
+          MainActivity.networkId = net.getId();
+        } catch (Exception e) {
+          Toast.makeText(this, "error while getting saved networks", Toast.LENGTH_SHORT).show();
+        }
+
+        return;
+      }
+
+      MainActivity.networkId = networkId;
+      long nodeId = Long.parseLong(nodeIdStr);
+
+      runOnNewThread(() -> {
+
+        Log.d(TAG, "onCreate: connecting with invite");
+
+        new Network.ConnectArgs()
+            .setNetworkId(networkId)
+            .setNodeId(nodeId)
+            .setInviteURL(inviteURL)
+            .setNetworkName("name")
+            .setNetworkType(Network.NetworkType.CALL_NET)
+            .setUserRole(Network.UserRole.OWNER)
+            .setAccessType(Network.AccessType.REMOTE)
+            .connect();
+
+      });
+    });
+
+    btnSendData.setOnClickListener(v -> {
+      String nodeIdStr = etDestNodeId.getEditableText().toString();
+
+      Log.d(TAG, "onCreate: using nodeId: " + nodeIdStr);
+
+      long[] nodeIds = null;
+
+      try {
+        long nodeId = Long.parseLong(nodeIdStr);
+        nodeIds = new long[] { nodeId };
+      } catch (NumberFormatException nfe) {
+        Log.d(TAG, "onCreate: err", nfe);
+      }
+
+      long[] finalNodeIds = nodeIds;
+      runOnNewThread(() -> {
+
+        Thread.sleep(5_000);
+
+        Network n = CocoClient.getInstance().getNetwork(networkId);
+
+        Log.d(TAG, "thirdStep: using network: " + n);
+
+        Objects.requireNonNull(n);
+
+        n.sendData("hello world", finalNodeIds);
+      });
+    });
+
+    btnSendInfo.setOnClickListener(v -> {
+      String nodeIdStr = etDestNodeId.getEditableText().toString();
+
+      Log.d(TAG, "onCreate: using nodeId: " + nodeIdStr);
+
+      long[] nodeIds = null;
+
+      try {
+        long nodeId = Long.parseLong(nodeIdStr);
+        nodeIds = new long[] { nodeId };
+      } catch (NumberFormatException nfe) {
+        Log.d(TAG, "onCreate: err", nfe);
+      }
+
+      long[] finalNodeIds = nodeIds;
+      runOnNewThread(() -> {
+
+        Thread.sleep(5_000);
+
+        Network n = CocoClient.getInstance().getNetwork(networkId);
+
+        Log.d(TAG, "thirdStep: using network: " + n);
+
+        Objects.requireNonNull(n);
+
+        n.sendContentInfo(System.currentTimeMillis(), "world hello", finalNodeIds);
+      });
+    });
+
+    messageLiveData.observe(this, message -> Toast.makeText(this, message, Toast.LENGTH_SHORT).show());
+
+    init(getFilesDir(), getString(R.string.client_id));
   }
 
-  private static void setTokens() {
-
-    try {
-
-      CocoClient.getInstance().setTokens("" +
-          "{\n" +
-          "    \"access_token\": \"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjYwZmNhOTgwZTc4MThiMDAxNjQyZWUwZCJ9.eyJpYXQiOjE2Mjc0MTM5NzYsImV4cCI6MTYyNzQxNzU3Niwic3ViIjoiNWViZDE1YTBlNmY0MmIwMDExN2RmMTBlIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwIiwiYXVkIjoiZjA3OTliZGVhM2FlYWRlYjc0MjkiLCJzY29wZSI6WyJhcHAubWdtdCIsImRldmljZS5tZ210IiwibmV0d29yay5tZ210IiwibmV0d29yay5hY2Nlc3MiLCJwcm9maWxlLmFjY2VzcyIsInByb2ZpbGUubWdtdCJdLCJwcm9maWxlIjp7InJvbGVzIjpbIkNvbnN1bWVyIiwiRGV2ZWxvcGVyIiwiT2VtIl19LCJvcGVyYXRvcklkIjoiNWU3MzM4MzhmNmE5NWUwMDE2ZTc5MDJmIiwianRpIjoiOGEwYTBlYjlmMTM0MzFhNCJ9.kjyfJmQpmKzkrgCFXWtssIVlr-nVbniievkusirAjcxi0sK_V1rynugMW6lGm4xLdcZ8QU-X_lfES0Rqho3qOPWQGBwi10PUZRbgU0L3r1fxI-5Df5nUuRwchpyNW8vNRF9zwCIhCahzg1DrdZJJ-gXGkS0gR7mSqRaqLHkc_Yy9Dw924vRaEQrd7dj0UNXtR3ATjhskNlPzEajyoo91qXeD03L986IQnk9lQu7Y4PKVrX0IpY_jIXLs3qOaz7MAsIyuH1ogaDY5dzLvGovQhiMHhTy2Ro9g7EKGksVzry2-4mbUHHG7dvIqlWJ40TpIWFDfn5kcALqAc6CrPS76TQ\",\n" +
-          "    \"refresh_token\": \"Dq1oYnEi0T7oT_-YTI0y\",\n" +
-          "    \"expires_in\": 3600,\n" +
-          "    \"token_type\": \"Bearer\"\n" +
-          "}"
-      );
-
-    } catch (Exception e) {
-      Log.e(TAG, "setTokens: error", e);
-    }
-  }
-
-  private static void firstStep(File dir, String clientId) {
-    Log.d(TAG, "firstStep: started");
-    Log.d(TAG, "firstStep: clientId: " + clientId);
+  private static void init(File dir, String clientId) {
+    Log.d(TAG, "init: started");
+    Log.d(TAG, "init: clientId: " + clientId);
 
     new CocoClient.Builder()
         .addCallbackListener(new DefaultNativeCallbacksInterface() {
 
           @Override
           public void connectStatusCallback(Network network, Object context) {
-            if (!network.getId().equals(NETWORK_ID))
-              return;
-
             Log.d(TAG, "connectStatusCallback: status: " + network.getState());
 
             if (!network.getState().isConnected())
               return;
 
-            runOnNewThread(MainActivity::fourthStep);
-          }
-
-          @Override
-          public void networkListCallback(ArrayList<Network> networksList, Object context) {
-            runOnNewThread(() -> thirdStep(networksList));
+            messageLiveData.postValue("connected");
           }
 
           @Override
           public void receiveDataCallback(Network network, long sourceNodeId, String data) {
-            Log.d(TAG, String.format("receiveDataCallback: nodeId: %d, data: %s", sourceNodeId, data));
+            String message = String.format(Locale.US, "receiveDataCallback: nodeId: %d, data: %s", sourceNodeId, data);
+            Log.d(TAG, message);
+
+            messageLiveData.postValue(message);
           }
 
           @Override
           public void contentInfoCallback(Network network, long sourceNodeId, long contentTime, String data) {
-            Log.d(TAG, String.format("contentInfoCallback: contentTime: %d, nodeId: %d, data: %s", contentTime, sourceNodeId, data));
+            String message = String.format(Locale.US, "contentInfoCallback: contentTime: %d, nodeId: %d, data: %s", contentTime, sourceNodeId, data);
+            Log.d(TAG, message);
+
+            messageLiveData.postValue(message);
           }
         })
         .withPlatform(new PlatformInterface() {
@@ -123,57 +253,10 @@ public class MainActivity extends AppCompatActivity {
           }
 
           @Override
-          public void OAuthCallback(String authorizationEndpoint, String tokenEndpoint) {
-            runOnNewThread(MainActivity::setTokens);
-          }
+          public void OAuthCallback(String authorizationEndpoint, String tokenEndpoint) {}
         })
         .build();
 
-    setTokens();
-
-    Log.d(TAG, "firstStep: completed");
-
-    runOnNewThread(MainActivity::secondStep);
-  }
-
-  private static void secondStep() throws Throwable {
-    Log.d(TAG, "secondStep: get all networks");
-    CocoClient.getInstance().getAllNetworks(null);
-  }
-
-  private static void thirdStep(List<Network> networks) {
-
-    try {
-      Thread.sleep(1_000);
-
-      if (null == networks) {
-        Log.e(TAG, "networkListCallback: error while get all networks");
-        return;
-      }
-
-      for (Network n : networks) {
-
-        if (n.getId().equals(NETWORK_ID)) {
-          n.connect();
-          break;
-        }
-      }
-    } catch (Throwable e) {
-      Log.w(TAG, "thirdStep: error", e);
-    }
-  }
-
-  private static void fourthStep() throws Exception {
-
-    Thread.sleep(5_000);
-
-    Network n = CocoClient.getInstance().getNetwork(NETWORK_ID);
-
-    Log.d(TAG, "fourthStep: using network: " + n);
-
-    Objects.requireNonNull(n);
-
-    n.sendData("hello world", null);
-    n.sendContentInfo(System.currentTimeMillis(), "world hello", null);
+    Log.d(TAG, "init: completed");
   }
 }
